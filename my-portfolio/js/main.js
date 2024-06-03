e30fe02f986fcb781e947623b971e75e0366e592
saku0802 <166917177+saku0802@users.noreply.github.com>
2024-06-03 21:49:41 +0900

    Update main.js

--- my-portfolio/js/main.js
+++ my-portfolio/js/main.js
@@ -1,14 +1,5 @@
 // main.js
 document.addEventListener('DOMContentLoaded', function() {
-  // ロード画面を非表示にする
-  const loading = document.getElementById('loading');
-  setTimeout(function() {
-    loading.style.opacity = '0';
-    setTimeout(function() {
-      loading.style.display = 'none';
-    }, 500);
-  }, 1500); // 1.5秒後にロード画面を非表示にする
-
   // スムーズスクロール
   const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
   smoothScrollLinks.forEach(link => {
@@ -22,13 +13,5 @@ document.addEventListener('DOMContentLoaded', function() {
         behavior: 'smooth'
       });
     });
-  });
-
-  // ハンバーガーメニュー
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('nav ul');
-  burger.addEventListener('click', function() {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
   });
 });
