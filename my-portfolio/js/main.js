9d49355f5d0fada66a20681f5585a4d1dab41251
saku0802 <166917177+saku0802@users.noreply.github.com>
2024-06-16 22:22:44 +0900

    Update main.js

--- my-portfolio/js/main.js
+++ my-portfolio/js/main.js
@@ -4,3 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.add('loaded');
     }, 300); // 少し遅らせて表示
 });
+// 言語切り替え機能は、後ほど実装します。
+console.log("JavaScriptファイルを読み込みました。");
