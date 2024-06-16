b36fab2a685543b8aef746c9ccfcab92494b94f9
saku0802 <166917177+saku0802@users.noreply.github.com>
2024-06-16 22:22:26 +0900

    Update main.js

--- my-portfolio/js/main.js
+++ my-portfolio/js/main.js
@@ -1,2 +1,6 @@
-// 言語切り替え機能は、後ほど実装します。
-console.log("JavaScriptファイルを読み込みました。");
+// ページ遷移アニメーション
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.body.classList.add('loaded');
+    }, 300); // 少し遅らせて表示
+});
