c3210db92459437dfc25957484f032a02b301704
saku0802 <166917177+saku0802@users.noreply.github.com>
2024-06-16 22:15:50 +0900

    Update main.js

--- my-portfolio/js/main.js
+++ my-portfolio/js/main.js
@@ -1,10 +1,2 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
+// 言語切り替え機能は、後ほど実装します。
+console.log("JavaScriptファイルを読み込みました。");
