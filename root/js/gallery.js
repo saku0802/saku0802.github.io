3e4d72d79292008cb1497471a51805bcc7bc95a3
user <user@appurunoMacBook-Pro.local>
2024-07-28 07:17:37 +0900

    プロジェクトのセクションを削除

--- root/js/gallery.js
+++ root/js/gallery.js
@@ -1,5 +1,6 @@
 const galleryFilter = document.querySelector('.gallery-filter');
 const galleryGrid = document.querySelector('.gallery-grid');
+const previewItems = document.querySelectorAll('.preview-item');
 
 galleryFilter.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
@@ -10,8 +11,8 @@ galleryFilter.addEventListener('click', (event) => {
     event.target.classList.add('active');
 
     // ギャラリーアイテムの表示/非表示を切り替え
-    galleryGrid.querySelectorAll('.hexagon').forEach(item => {
-      if (filterValue === 'all' || item.dataset.year === filterValue || item.dataset.theme === filterValue) {
+    previewItems.forEach(item => {
+      if (filterValue === 'all' || item.dataset.theme === filterValue) {
         item.style.display = 'block';
       } else {
         item.style.display = 'none';
@@ -20,23 +21,22 @@ galleryFilter.addEventListener('click', (event) => {
   }
 });
 
-// ヘキサゴンの回転アニメーション
-const hexagons = document.querySelectorAll('.hexagon');
-
-hexagons.forEach(hexagon => {
-  hexagon.addEventListener('mouseover', () => {
-    hexagon.style.transform = 'rotateY(180deg)';
-  });
-
-  hexagon.addEventListener('mouseout', () => {
-    hexagon.style.transform = 'rotateY(0deg)';
-  });
-
+// プレビュー画像とモーダルの処理
+previewItems.forEach(item => {
+  const imageLink = item.querySelector('a');
   // モーダルウィンドウ
-  const modal = document.getElementById(hexagon.querySelector('img').dataset.target);
+  const modal = document.getElementById(imageLink.getAttribute('href').substring(1)); // ここでsubstring(1)を適用
   const closeBtn = modal.querySelector('.close-btn');
+  const modalImage = modal.querySelector('img');
+  const modalCaption = modal.querySelector('.modal-caption');
 
-  hexagon.addEventListener('click', () => {
+  imageLink.addEventListener('click', (event) => {
+    event.preventDefault(); // リンクのデフォルト動作を無効化
+
+    // モーダルにタイトルと説明を設定
+    modalCaption.querySelector('h3').textContent = imageLink.dataset.title;
+    modalCaption.querySelector('p').textContent = imageLink.dataset.description;
+
     modal.style.display = 'block';
   });
 
